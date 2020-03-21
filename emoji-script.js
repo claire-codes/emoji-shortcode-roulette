c557d04380d8aa4325adf32b32eeb13d703f3640
claire-codes <claire-codes@users.noreply.github.com>
2020-03-21 16:51:45 +0000

    Show a random emoji as page loads

--- emoji-script.js
+++ emoji-script.js
@@ -1,23 +1,29 @@
 let request = new XMLHttpRequest();
 let emojiData;
+let button = document.querySelector(".js-button");
+let image = document.querySelector(".js-image");
+let shortCode = document.querySelector(".js-shortcode");
+
 let getRandomProperty = (obj = {}) => {
   let keys = Object.keys(obj);
   let randomKey = keys[(keys.length * Math.random()) << 0];
   return { code: randomKey, imageSrc: obj[randomKey] };
 };
 
+let updateEmojiOnScreen = (emojiData = {imageSrc: '', code: ''}) => {
+    image.src = emojiData["imageSrc"];
+    shortCode.innerHTML = emojiData["code"];
+}
+
 request.open("GET", "github-emoji.json");
 request.onload = function() {
   emojiData = JSON.parse(this.response);
+  updateEmojiOnScreen(getRandomProperty(emojiData));
 };
 request.send();
 
-let button = document.querySelector(".js-button");
-let image = document.querySelector(".js-image");
-let shortCode = document.querySelector(".js-shortcode");
 button.addEventListener("click", () => {
   let result = getRandomProperty(emojiData);
   console.log(result);
-  image.src = result["imageSrc"];
-  shortCode.innerHTML = result["code"];
+  updateEmojiOnScreen(result);
 });

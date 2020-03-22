634a78811dda8f3ab874f5513e6c44421b05a8e6
claire-codes <claire-codes@users.noreply.github.com>
2020-03-22 22:27:37 +0000

    Renaming files and giving things titles and formatting

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(9999, function(){
-    console.log('Server running on http://localhost:9999/');
-});
+var connect = require("connect");
+var serveStatic = require("serve-static");
+connect()
+  .use(serveStatic(__dirname))
+  .listen(9999, function() {
+    console.log("Server running on http://localhost:9999/");
+  });

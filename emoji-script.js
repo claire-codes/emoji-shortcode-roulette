5a33fd5483e557299e0ec7653b3dae613a5e2fac
claire-codes <claire-codes@users.noreply.github.com>
2020-03-22 20:17:55 +0000

    Add copy to clipboard button

--- emoji-script.js
+++ emoji-script.js
@@ -27,3 +27,20 @@ button.addEventListener("click", () => {
   console.log(result);
   updateEmojiOnScreen(result);
 });
+
+// Clipboard.js stuff
+
+var clipboard = new ClipboardJS('.js-copy');
+
+clipboard.on('success', function(e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+
+    e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+});
